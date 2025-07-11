7cb7ab55aaa954b8f71947f86ece89828cc7cb58
ChandramohithP <chandramohithp@gmail.com>
2025-07-11 05:58:19 +0530

    week 4 hanson 2 springrest

---
+++ week4/Spring_rest_handson2/spring-learn-rest-api/src/main/java/com/cognizant/spring_learn/service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.cognizant.spring_learn.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CountryNotFoundException.class)
+    public ResponseEntity<Object> handleCountryNotFoundException(CountryNotFoundException ex) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.NOT_FOUND.value());
+        body.put("error", "Not Found");
+        body.put("message", ex.getMessage());
+        body.put("path", "/countries/{code}");
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+}
