f4626d84d187c443661dd3e19253c6bbd85d79e0
ChandramohithP <chandramohithp@gmail.com>
2025-07-13 20:11:16 +0530

    week 4 handson5 jwt pro 1 spring security

---
+++ week4/JWT_spring_sec_pro_1_2_3/jwt_pro_1/spring-security-basic-auth/src/main/java/com/example/demo/controller/CountryController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
+@RestController
+public class CountryController {
+    @GetMapping("/countries")
+    public List<Map<String, String>> getCountries() {
+        List<Map<String, String>> countries = new ArrayList<>();
+        Map<String, String> us = new HashMap<>();
+        us.put("code", "US");
+        us.put("name", "United States");
+        Map<String, String> in = new HashMap<>();
+        in.put("code", "IN");
+        in.put("name", "India");
+        Map<String, String> jp = new HashMap<>();
+        jp.put("code", "JP");
+        jp.put("name", "Japan");
+        countries.add(us);
+        countries.add(in);
+        countries.add(jp);
+        return countries;
+    }
+
+}
