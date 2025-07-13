7af933a0a33377b6dcb145f9981e208707ef26dd
ChandramohithP <chandramohithp@gmail.com>
2025-07-13 21:16:23 +0530

    week 4 handson5 jwt pro 2 spring security user roles

---
+++ week4/JWT_spring_sec_pro_1_2_3/jwt_pro_2/spring-security-users-roles/src/main/java/com/example/demo/SpringSecurityUsersRolesApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityUsersRolesApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringSecurityUsersRolesApplication.class, args);
+    }
+}
